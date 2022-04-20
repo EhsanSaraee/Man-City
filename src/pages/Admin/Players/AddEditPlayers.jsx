7016b7ad0ef257fa2f7490d71fe33d12dc882595
ehsan <ehsansaraee6570@yahoo.com>
2022-04-20 20:02:13 +0430

    implementing add & edit player functionality

--- src/pages/Admin/Players/AddEditPlayers.jsx
+++ src/pages/Admin/Players/AddEditPlayers.jsx
@@ -2,7 +2,7 @@ import { useFormik } from 'formik';
 import { useEffect, useState } from 'react';
 import AdminLayout from '../../../HOC/AdminLayout';
 import * as yup from 'yup';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import {
    Button,
    FormControl,
@@ -11,11 +11,14 @@ import {
    TextField,
 } from '@material-ui/core';
 import {
+   errorToast,
    isSelectError,
    selectErrorHelper,
+   successToast,
    textErrorHelper,
 } from '../../../utils/tools';
 import { EditPlayersDialogWrapper } from './styles';
+import { playersCollection } from '../../../firebase';
 
 const defaultValues = {
    name: '',
@@ -26,6 +29,7 @@ const defaultValues = {
 
 const AddEditPlayers = () => {
    const { playerID } = useParams();
+   const navigate = useNavigate();
 
    const [loading, setLoading] = useState(false);
    const [formType, setFormType] = useState('');
@@ -40,16 +44,54 @@ const AddEditPlayers = () => {
          number: yup
             .number()
             .required('Number is required')
-            .min('0', 'Number must be greater than 0')
-            .max('100', 'Number must be less than 100'),
+            .min(0, 'Number must be greater than 0')
+            .max(100, 'Number must be less than 100'),
          position: yup.string().required('Position is required'),
       }),
+      onSubmit: (values) => submitForm(values),
    });
+
+   const submitForm = (values) => {
+      let formData = values;
+      setLoading(true);
+
+      if (formType === 'add') {
+         playersCollection
+            .add(formData)
+            .then(() => {
+               successToast('Player added successfully');
+               formik.resetForm();
+               navigate('/admin_players');
+            })
+            .catch((error) => errorToast(error));
+      } else {
+         playersCollection
+            .doc(playerID)
+            .update(formData)
+            .then(() => {
+               successToast('Player updated successfully');
+               navigate('/admin_players');
+            })
+            .catch((error) => errorToast(error))
+            .finally(() => setLoading(false));
+      }
+   };
 
    useEffect(() => {
       if (playerID) {
-         setFormType('edit');
-         setValues({ name: 'Simon' });
+         playersCollection
+            .doc(playerID)
+            .get()
+            .then((snapshot) => {
+               const player = snapshot.data();
+               if (player) {
+                  setFormType('edit');
+                  setValues(player);
+               } else {
+                  errorToast('Player not found');
+               }
+            })
+            .catch((error) => errorToast(error));
       } else {
          setFormType('add');
          setValues(defaultValues);
