a7db1f0acd8fe1aea270ac10a74e2f444721bdd6
ehsan <ehsansaraee6570@yahoo.com>
2022-04-23 22:09:00 +0430

    add 7 edit functionality added

--- src/pages/Admin/Matches/AddEditMatch.jsx
+++ src/pages/Admin/Matches/AddEditMatch.jsx
@@ -1,7 +1,14 @@
-import { FormControl, MenuItem, Select, TextField } from '@material-ui/core';
+import {
+   Button,
+   Divider,
+   FormControl,
+   MenuItem,
+   Select,
+   TextField,
+} from '@material-ui/core';
 import { useFormik } from 'formik';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import * as yup from 'yup';
 import { matchesCollection, teamsCollection } from '../../../firebase';
 import AdminLayout from '../../../HOC/AdminLayout';
@@ -10,6 +17,7 @@ import {
    isSelectError,
    selectErrorHelper,
    textErrorHelper,
+   successToast,
 } from '../../../utils/tools';
 import { EditMatchDialogWrapper } from './styles';
 
@@ -32,6 +40,7 @@ const AddEditMatch = () => {
    const [values, setValues] = useState(defaultValues);
 
    const { matchID } = useParams();
+   const navigate = useNavigate();
 
    const formik = useFormik({
       enableReinitialize: true,
@@ -63,7 +72,7 @@ const AddEditMatch = () => {
       }),
       onSubmit: (values) => {
          // submit form
-         console.log(values);
+         submitForm(values);
       },
    });
 
@@ -75,6 +84,50 @@ const AddEditMatch = () => {
               </MenuItem>
            ))
          : null;
+
+   const submitForm = (values) => {
+      let dataToSubmit = values;
+
+      teams.forEach((team) => {
+         if (team.shortName === dataToSubmit.local) {
+            dataToSubmit['localThmb'] = team.thmb;
+         }
+         if (team.shortName === dataToSubmit.away) {
+            dataToSubmit['awayThmb'] = team.thmb;
+         }
+      });
+
+      setLoading(true);
+      if (formType === 'add') {
+         matchesCollection
+            .add(dataToSubmit)
+            .then(() => {
+               successToast('Match added :)');
+               formik.resetForm();
+               navigate('/admin_matches');
+            })
+            .catch((error) => {
+               errorToast('Sorry, something went wrong', error);
+            })
+            .finally(() => {
+               setLoading(false);
+            });
+      } else {
+         matchesCollection
+            .doc(matchID)
+            .update(dataToSubmit)
+            .then(() => {
+               successToast('Match Updated');
+               navigate('/admin_matches');
+            })
+            .catch((error) => {
+               errorToast('Sorry, something went wrong', error);
+            })
+            .finally(() => {
+               setLoading(false);
+            });
+      }
+   };
 
    useEffect(() => {
       if (!teams) {
@@ -115,13 +168,13 @@ const AddEditMatch = () => {
    }, [matchID]);
 
    return (
-      <AdminLayout title="undefined">
+      <AdminLayout title={formType === 'add' ? 'Add match' : 'Edit match'}>
          <EditMatchDialogWrapper>
             <div>
                <form onSubmit={formik.handleSubmit}>
                   <div>
-                     <h4>Select date</h4>
-                     <FormControl>
+                     <h4 style={{ marginBottom: '10px' }}>Select date</h4>
+                     <FormControl style={{ marginBottom: '10px' }}>
                         <TextField
                            id="date"
                            name="date"
@@ -133,11 +186,14 @@ const AddEditMatch = () => {
                      </FormControl>
                   </div>
 
-                  <hr />
+                  <Divider style={{ marginBottom: '10px' }} />
 
                   <div>
-                     <h4>Result local</h4>
-                     <FormControl error={isSelectError(formik, 'local')}>
+                     <h4 style={{ marginBottom: '10px' }}>Result local</h4>
+                     <FormControl
+                        style={{ marginBottom: '10px' }}
+                        error={isSelectError(formik, 'local')}
+                     >
                         <Select
                            id="local"
                            name="local"
@@ -153,7 +209,9 @@ const AddEditMatch = () => {
                         {selectErrorHelper(formik, 'local')}
                      </FormControl>
 
-                     <FormControl style={{ marginLeft: '10px' }}>
+                     <FormControl
+                        style={{ marginBottom: '10px', marginLeft: '10px' }}
+                     >
                         <TextField
                            id="resultLocal"
                            name="resultLocal"
@@ -166,8 +224,11 @@ const AddEditMatch = () => {
                   </div>
 
                   <div>
-                     <h4>Result away</h4>
-                     <FormControl error={isSelectError(formik, 'away')}>
+                     <h4 style={{ marginBottom: '10px' }}>Result away</h4>
+                     <FormControl
+                        style={{ marginBottom: '10px' }}
+                        error={isSelectError(formik, 'away')}
+                     >
                         <Select
                            id="away"
                            name="away"
@@ -183,7 +244,9 @@ const AddEditMatch = () => {
                         {selectErrorHelper(formik, 'away')}
                      </FormControl>
 
-                     <FormControl style={{ marginLeft: '10px' }}>
+                     <FormControl
+                        style={{ marginBottom: '10px', marginLeft: '10px' }}
+                     >
                         <TextField
                            id="resultAway"
                            name="resultAway"
@@ -193,6 +256,86 @@ const AddEditMatch = () => {
                            {...textErrorHelper(formik, 'resultAway')}
                         />
                      </FormControl>
+                  </div>
+
+                  <Divider style={{ marginBottom: '10px' }} />
+
+                  <div>
+                     <h4 style={{ marginBottom: '10px' }}>Match info</h4>
+                     <div style={{ marginBottom: '5px' }}>
+                        <FormControl>
+                           <TextField
+                              id="referee"
+                              name="referee"
+                              variant="outlined"
+                              placeholder="Add the referee name"
+                              {...formik.getFieldProps('referee')}
+                              {...textErrorHelper(formik, 'referee')}
+                           />
+                        </FormControl>
+                     </div>
+
+                     <div style={{ marginBottom: '5px' }}>
+                        <FormControl>
+                           <TextField
+                              id="stadium"
+                              name="stadium"
+                              variant="outlined"
+                              placeholder="Add the stadium name"
+                              {...formik.getFieldProps('stadium')}
+                              {...textErrorHelper(formik, 'stadium')}
+                           />
+                        </FormControl>
+                     </div>
+
+                     <div style={{ marginBottom: '5px' }}>
+                        <FormControl error={isSelectError(formik, 'result')}>
+                           <Select
+                              id="result"
+                              name="result"
+                              variant="outlined"
+                              displayEmpty
+                              {...formik.getFieldProps('result')}
+                           >
+                              <MenuItem value="" disabled>
+                                 Select a result
+                              </MenuItem>
+                              <MenuItem value="W">Win</MenuItem>
+                              <MenuItem value="D">Draw</MenuItem>
+                              <MenuItem value="L">Lose</MenuItem>
+                              <MenuItem value="n/a">Non available</MenuItem>
+                           </Select>
+                           {selectErrorHelper(formik, 'result')}
+                        </FormControl>
+                     </div>
+
+                     <div style={{ marginBottom: '5px' }}>
+                        <FormControl error={isSelectError(formik, 'final')}>
+                           <Select
+                              id="final"
+                              name="final"
+                              variant="outlined"
+                              displayEmpty
+                              {...formik.getFieldProps('final')}
+                           >
+                              <MenuItem value="" disabled>
+                                 Was the game played ?
+                              </MenuItem>
+                              <MenuItem value="yes">Yes</MenuItem>
+                              <MenuItem value="no">No</MenuItem>
+                           </Select>
+                           {selectErrorHelper(formik, 'final')}
+                        </FormControl>
+                     </div>
+
+                     <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        disabled={loading}
+                     >
+                        {formType === 'add' ? 'Add match' : 'Edit match'}
+                     </Button>
                   </div>
                </form>
             </div>
